36deea3bdc70948be8230a48a1ba8337fc1175ef
SnejUgal <snejugal@icloud.com>
2018-03-04 22:29:36 +0700

    development environment

---
+++ rules.js
@@ -0,0 +1,11 @@
+/**
+ * All rules accept only one argument with themes and returns either true
+ * indicating everything is okay or an object {
+ *  type: `error`/`warning`,
+ *  title: string,
+ *  description: string,
+ *  variables: string[],
+ * }.
+ */
+
+module.exports = [];
