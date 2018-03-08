a9fc8fae29224fde105fd6038881be5f869dd4d1
SnejUgal <snejugal@icloud.com>
2018-03-08 17:42:23 +0700

    debugging

---
+++ debug.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+global.Attheme = require(`attheme-js`);
+global.color = require(`./color`);
+global.index = require(`./index`);
+global.rules = require(`./rules`);
+
+setInterval(() => { }, 2 ** 32 - 1);
